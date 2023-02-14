d38dab708a113a310e9fc27d270c80819405c34a
YsdLabo <108654567+YsdLabo@users.noreply.github.com>
2023-02-15 05:41:36 +0900

    Update twist_to_driving.cpp

--- driving_controller_pkg/src/twist_to_driving.cpp
+++ driving_controller_pkg/src/twist_to_driving.cpp
@@ -24,7 +24,7 @@ private:
 	//
 	double course;
 	double speed;
-	double rotate;
+	double rotation;
 	
 	// enum
 	enum class E_STATE { NONE, IDLING, STEERING, RUNNING, STOPPING };
@@ -46,10 +46,8 @@ private:
 		ROTATION_L,
 		ROTATION_R
 	};
-	E_STEERING steering_dir = E_STEERING::NONE;
 	E_STEERING steering_dir_now;
 	E_STEERING stored_steering_dir;
-	E_STEERING steering_dir_old;
 	
 public:
 	void onInit()
@@ -75,7 +73,7 @@ private:
 		course = std::atan2(y, x);
 		speed  = std::sqrt(x*x+y*y);
 		if(speed > MAX_SPEED) speed = MAX_SPEED;
-		rotate = w;
+		rotation = w;
 	}
 	
 	void stm_callback(const ros::TimerEvent& e)
@@ -109,14 +107,14 @@ private:
 			steering_dir_now = E_STEERING::DIRECTION_STOP;
 		}
 		else {
-			if( rotate_is_zero() ) {
-				if( in_area_F() ) steering_dir_now = E_STEERING::DIRECTION_F;
-				else if( in_area_L() ) steering_dir_now = E_STEERING::DIRECTION_L;
-				else if( in_area_R() ) steering_dir_now = E_STEERING::DIRECTION_R;
-				else if( in_area_FL() ) steering_dir_now = E_STEERING::DIRECTION_FL;
-				else if( in_area_BL() ) steering_dir_now = E_STEERING::DIRECTION_BL;
-				else if( in_area_FR() ) steering_dir_now = E_STEERING::DIRECTION_FR;
-				else if( in_area_BR() ) steering_dir_now = E_STEERING::DIRECTION_BR;
+			if( rotation_is_zero() ) {
+				if( course_is_in_area_F() ) steering_dir_now = E_STEERING::DIRECTION_F;
+				else if( course_is_in_area_L() ) steering_dir_now = E_STEERING::DIRECTION_L;
+				else if( course_is_in_area_R() ) steering_dir_now = E_STEERING::DIRECTION_R;
+				else if( course_is_in_area_FL() ) steering_dir_now = E_STEERING::DIRECTION_FL;
+				else if( course_is_in_area_BL() ) steering_dir_now = E_STEERING::DIRECTION_BL;
+				else if( course_is_in_area_FR() ) steering_dir_now = E_STEERING::DIRECTION_FR;
+				else if( course_is_in_area_BR() ) steering_dir_now = E_STEERING::DIRECTION_BR;
 				else steering_dir_now = E_STEERING::DIRECTION_B;
 			}
 			else {
@@ -126,19 +124,152 @@ private:
 		}
 	}
 	
-	constexpr double AREA_F_L = M_PI / 8.0;
-	constexpr double AREA_F_R = - M_PI / 8.0;
-	constexpr double AREA_B_L = M_PI * 7.0 / 8.0;
-	constexpr double AREA_B_R = - M_PI * 7.0 / 8.0;
-	constexpr double AREA_L_F = M_PI * 3.0 / 8.0;
-	constexpr double AREA_L_B = M_PI * 5.0 / 8.0;
-	constexpr double AREA_R_F = - M_PI * 3.0 / 8.0;
-	constexpr double AREA_R_B = - M_PI * 5.0 / 8.0;
+	constexpr double AREA_F_L = M_PI / 8.0;			// 22.5
+	constexpr double AREA_L_F = M_PI * 3.0 / 8.0;		// 67.5
+	constexpr double AREA_L_B = M_PI * 5.0 / 8.0;		// 112.5
+	constexpr double AREA_B_L = M_PI * 7.0 / 8.0;		// 157.5
+	constexpr double AREA_F_R = - M_PI / 8.0;		// -22.5
+	constexpr double AREA_R_F = - M_PI * 3.0 / 8.0;		// -67.5
+	constexpr double AREA_R_B = - M_PI * 5.0 / 8.0;		// -112.5
+	constexpr double AREA_B_R = - M_PI * 7.0 / 8.0;		// -157.5
 	
-	bool in_area_F()
+	bool course_is_in_area_F()
 	{
-		return (course >= AREA_F_R && course <= AREA_F_L);
+		return (course >= AREA_F_R && course <= AREA_F_L);  // -22.5〜22.5
 	}
+	bool course_is_in_area_FL()
+	{
+		return (course > AREA_F_L && course < AREA_L_F);  // 22.5〜67.5
+	}
+	bool course_is_in_area_L()
+	{
+		return (course >= AREA_L_F && course <= AREA_L_B);  // 67.5〜112.5
+	}
+	bool course_is_in_area_BL()
+	{
+		return (course > AREA_L_B && course < AREA_B_L);  // 112.5〜157.5
+	}
+	bool course_is_in_area_FR()
+	{
+		return (course > AREA_R_F && course < AREA_F_R);  // -67.5〜-22.5
+	}
+	bool course_is_in_area_R()
+	{
+		return (course >= AREA_R_B && course <= AREA_R_F);  // -112.5〜-67.5
+	}
+	bool course_is_in_area_BR()
+	{
+		return (course > AREA_B_R && course < AREA_R_B);  // -157.5〜-112.5
+	}
+	bool speed_is_zero()
+	{
+		return (speed < 0.01);
+	}
+	bool rotation_is_zero()
+	{
+		return (rotation < 0.1);
+	}
+	void store_current_steering_dir()
+	{
+		stored_steering_dir = steering_dir_now;
+	}
+	bool course_changed()
+	{
+		return (steering_dir_now != stored_steering_dir);
+	}
+	
+	bool check_all_motors_stopped()
+	{
+		return motor_controller.check_stopped_motors();
+	}
+	void start_steering()
+	{
+		motor_controller.steer(static_cast<double>(steering_dir_now));
+	}
+	void start_steering()
+	{
+		motor_controller.move(static_cast<double>(steering_dir_now), speed);
+	}
+	void stop_steering()
+	{
+		motor_controller.stop();
+	}
+	
+	// アイドリング中
+	void state_idling()
+	{
+		if(action == E_ACTION::ENTRY) {
+			store_current_steering_dir();
+			action = E_ACTION::DO;
+		}
+		if(action == E_ACTION::DO) {
+			if( course_changed() ) {
+				main_state = E_STATE::STEERING;
+				action = E_ACTION::EXIT;
+			}
+		}
+		if(action == E_ACTION::EXIT) {
+			action = E_ACTION::ENTRY;
+		}
+	}
+	
+	// 操舵中
+	void state_steering()
+	{
+		if(action == E_ACTION::ENTRY) {
+			store_current_steering_dir();
+			start_steering();
+			action = E_ACTION::DO;
+		}
+		if(action == E_ACTION::DO) {
+			if(check_all_motors_stopped()) {
+				if(course_changed()) main_state = E_STATE::STEERING;
+				else main_state = E_STATE::RUNNING;
+				action = E_ACTION::EXIT;
+			}
+		}
+		if(action == E_ACTION::EXIT) {
+			action = E_ACTION::ENTRY;
+		}
+	}
+
+	// 走行中
+	void state_running()
+	{
+		if(action == E_ACTION::ENTRY) {
+			// store_current_steering_dir();
+			action = E_ACTION::DO;
+		}
+		if(action == E_ACTION::DO) {
+			start_running(static_cast<double>(steering_dir_now), speed);
+			if(course_changed()) {
+				main_state = E_STATE::STOPPING;
+				action = E_ACTION::EXIT;
+			}
+		}
+		if(action == E_ACTION::EXIT) {
+			action = E_ACTION::ENTRY;
+		}
+	}
+
+	// 停止中
+	void state_stopping()
+	{
+		if(action == E_ACTION::ENTRY) {
+			stop_running();
+			action = E_ACTION::DO;
+		}
+		if(action == E_ACTION::DO) {
+			if(check_all_motors_stopped()) {
+				main_state = E_STATE::IDLING;
+				action = E_ACTION::EXIT;
+			}
+		}
+		if(action == E_ACTION::EXIT) {
+			action = E_ACTION::ENTRY;
+		}
+	}
+
 };
 
 }
