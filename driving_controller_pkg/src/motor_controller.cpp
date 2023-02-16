6f9cf1cd8ad3d425df85aba6f292606e09ff7ca4
YsdLabo <108654567+YsdLabo@users.noreply.github.com>
2023-02-16 21:39:33 +0900

    Update motor_controller.cpp

--- driving_controller_pkg/src/motor_controller.cpp
+++ driving_controller_pkg/src/motor_controller.cpp
@@ -5,16 +5,34 @@ class MotorController
 private:
   ros::NodeHandle nh;
   
+  int steering_angle[5][] = {
+    {0, 0, 0, 0},  // course F 0[deg], B 180[deg], stop
+    {45, 45, 45, 45},  // course FL 45[deg], BR -135[deg]
+    {-90, 90, -90, 90},  // course L 90[deg], R -90[deg]
+    {-45, -45, -45, -45},  // course BL 135[deg], FR -45[deg]
+    {-45, 45, -45, 45},  // rotation L, R
+  };
+  
 public:
   void init()
   {
   }
   
-  void steer()
+  void steer(int course)
   {
+    switch(course) {
+      case 0:
+        for(int i=0;i<4;i++) {
+          servo[i].move(steering_angle[course][i]);   // 絶対角
+          ultra[i].move(steering_angle[course][i+4]); // 相対角
+        }
+        break;
+      case 45:
+        break;
+    }
   }
   
-  void move()
+  void move(int course, double speed)
   {
   }
   
