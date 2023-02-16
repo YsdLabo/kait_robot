ac0f0841332c62183b1a005a94069a72c527c91d
YsdLabo <108654567+YsdLabo@users.noreply.github.com>
2023-02-16 20:58:03 +0900

    Create motor_controller.cpp

---
+++ driving_controller_pkg/src/motor_controller.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+
+class MotorController
+{
+private:
+  ros::NodeHandle nh;
+  
+public:
+  void init()
+  {
+  }
+  
+  void steer()
+  {
+  }
+  
+  void move()
+  {
+  }
+  
+  void stop()
+  {
+  }
+  
+  bool check_stopped_motors()
+  {
+  }
+};
