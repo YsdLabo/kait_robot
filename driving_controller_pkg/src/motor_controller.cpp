6537af68468e19c35799685c570e2abc770bb3a7
YsdLabo <108654567+YsdLabo@users.noreply.github.com>
2023-03-09 04:13:09 +0900

    Update motor_controller.cpp

--- driving_controller_pkg/src/motor_controller.cpp
+++ driving_controller_pkg/src/motor_controller.cpp
@@ -1,4 +1,18 @@
 #include<ros/ros.h>
 #include<nodelet/nodelet.h>
 
-class 
+namespace driving_controller_ns
+{
+  class MotorController
+  {
+    private:
+    ros::NodeHandle nh;
+    ros::serverService serverDrivingState;
+    ros::serverService serverStoppedState;
+    
+    public:
+    void onInit()
+    {
+    }
+  };
+}
