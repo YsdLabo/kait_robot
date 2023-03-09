2a7a2e0d47c4d49ecdf414f0677a5afe980c3e3e
YsdLabo <108654567+YsdLabo@users.noreply.github.com>
2023-03-10 04:23:40 +0900

    Update steering_controller.cpp

--- driving_controller_pkg/src/steering_controller.cpp
+++ driving_controller_pkg/src/steering_controller.cpp
@@ -11,19 +11,33 @@ class SteeringController
     {-90, 90, -90, 90},
     {-45, 45, -45, 45}
   };
+  int servo_angle_now[4];
   
   ICSData ics_data;
   
   void drive_servo(int motor_id, int angle, int speed)
   {
     ics_set_speed(&ics_data, motor_id, speed);
-    int pulse = (int)(angle * 4000.0 / 135.0) + 7500;
+    int pulse = angle_to_pulse(angle);
     ics_pos(&ics_data, motor_id, pulse);
   }
   
   void drive_piezo(int motor_id, int speed)
   {
-    
+    // 0:-, 1:+, 2:+, 3:-
+    if(motor_id == 0 || motor_id == 3) {
+      
+    }
+    else {
+    }
+  }
+  
+  int angle_to_pulse(int angle)
+  {
+    int pulse = (int)(angle * 4000.0 / 135.0) + 7500;
+    if(pulse < 3500) pulse = 3500;
+    if(pulse > 11500) pulse = 11500;
+    return pulse;
   }
   
   public:
@@ -31,14 +45,23 @@ class SteeringController
     ics_init(&ics_data);
   }
   
+  ~SteeringController() {
+    ics_close(&ics_data);
+  }
+  
   void steering(int steer_next)
   {
     int amount[4];
     for(int i=0;i<4;i++) {
-      amount[i] = steering_angle[steer_next][i] - steering_angle[steer_now][i];
+      amount[i] = (steering_angle[steer_next][i] - steering_angle[steer_now][i]) / 45;
     }
-    drive_servo(0, steering_angle[steer_next][i], amount[i]/45);
-    piezo();
+    drive_servo(i, steering_angle[steer_next][i], amount[i]);
+    drive_piezo(i, amount[i]);
     steer_now = steer_next;
+  }
+  
+  void running(double speed)
+  {
+    // forward  0:+ 1:- 2:- 3:+
   }
 };
