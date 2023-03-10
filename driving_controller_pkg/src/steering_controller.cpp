192f60a3efd7687bbe364a072befbff7e561ff86
YsdLabo <108654567+YsdLabo@users.noreply.github.com>
2023-03-10 21:31:35 +0900

    Update steering_controller.cpp

--- driving_controller_pkg/src/steering_controller.cpp
+++ driving_controller_pkg/src/steering_controller.cpp
@@ -18,21 +18,31 @@ class SteeringController
   ICSData ics_data;
   PiezoSonic piezo[4];
   
-  void drive_servo(int motor_id, int angle, int speed)
+  // for running
+  constexpr double  alpha = 0.9;
+  constexpr double  beta = 0.95;
+  constexpr double  mps_to_digit = 1000.0;
+  double speed_d = 0;
+  
+  // Subscribe joint state
+  ros::NodeHandle nh;
+  ros::Subscriber joint_state_sub;
+  
+  void steer_servo(int motor_id, int angle, int speed)
   {
-    ics_set_speed(&ics_data, motor_id, speed);
+    ics_set_speed(&ics_data, motor_id, std::abs(speed) * 20);
     int pulse = angle_to_pulse(angle);
     ics_pos(&ics_data, motor_id, pulse);
   }
   
-  void drive_piezo(int motor_id, int speed)
+  void steer_piezo(int motor_id, int speed)
   {
+    int speed_m = speed;
     // 0:-, 1:+, 2:+, 3:-
     if(motor_id == 0 || motor_id == 3) {
-      
+      speed_m *= -1;
     }
-    else {
-    }
+    piezo[motor_id].move(speed_m);
   }
   
   int angle_to_pulse(int angle)
@@ -47,6 +57,8 @@ class SteeringController
   SteeringController() {
     ics_init(&ics_data);
     for(int i=0;i<4;i++) piezo[i].open(i);
+    nh = getNodeHandle();
+    joint_state_sub = nh.subscribe("odom", 10, &Steering
   }
   
   ~SteeringController() {
@@ -59,15 +71,24 @@ class SteeringController
   {
     int amount[4];
     for(int i=0;i<4;i++) {
-      amount[i] = (steering_angle[steer_next][i] - steering_angle[steer_now][i]) ;
+      amount[i] = (steering_angle[steer_next][i] - steering_angle[steer_now][i]) / 45;
+      steer_servo(i, steering_angle[steer_next][i], amount[i]);
+      steer_piezo(i, amount[i]);
     }
-    drive_servo(i, steering_angle[steer_next][i], amount[i]);
-    drive_piezo(i, amount[i]);
     steer_now = steer_next;
   }
   
-  void running(double speed)
+  void running(double speed_ms)
   {
+    double speed = speed_ms * mps_to_digit;
+    // Low-pass
+    if(speed == 0) speed_d = beta * speed_d + (1.0-beta) * speed;
+    else speed_d = alpha * speed_d + (1.0-alpha) * speed;
+    
     // forward  0:+ 1:- 2:- 3:+
+    piezo[0].move(speed_d);
+    piezo[1].move(-1*speed_d);
+    piezo[2].move(-1*speed_d);
+    piezo[3].move(speed_d);
   }
 };
