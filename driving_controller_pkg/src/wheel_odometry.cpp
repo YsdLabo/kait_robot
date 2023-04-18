aabb4903a10894b20aa44b3f7c590cff626655af
YsdLabo <108654567+YsdLabo@users.noreply.github.com>
2023-04-18 19:56:30 +0900

    Update wheel_odometry.cpp

--- driving_controller_pkg/src/wheel_odometry.cpp
+++ driving_controller_pkg/src/wheel_odometry.cpp
@@ -1,3 +1,12 @@
+class WheelOdometry
+{
+private:
+	double L1 = 0.4 + 0.1;
+	double L2 = + 0.1
+	double L3 = 
+public:
+};
+
 void WheelOdometry::run(double diff_wheel[4], double steer[4])
 {
 	// Wheel Odometry for 4WS
@@ -17,19 +26,57 @@ void WheelOdometry::run(double diff_wheel[4], double steer[4])
 	// 操舵方向に合わせて，オドメトリ
 	if(steering_dir == 0)
 	{
+		// Forward
+		if(diff_wheel[0] > 0) dth = (diff_wheel[1] - diff_wheel[0]) / L1;
+		// Back
+		else dth = (diff_wheel[3] - diff_wheel[2]) / L1;
+		dv = 0.0;
+		for(int i=0;i<4;i++) dv += diff_wheel[i];
+		dv /= 4.0;
+		phi = 0.0;
 	}
 	else if(steering_dir == 1)
 	{
+		// Forward Left
+		if(diff_wheel[0] > 0) dth = (diff_wheel[1] - diff_whee[3]) / L2;
+		// Backward Right
+		else dth = (diff_wheel[3] - diff_wheel[1]) / L2;
+		dv = 0.0;
+		for(int i=0;i<4;i++) dv += diff_wheel[i];
+		dv /= 4.0;
+		phi = M_PI / 4.0;
 	}
 	else if(steering_dir == 2)
 	{
+		// Forward Right
+		if(diff_wheel[0] > 0) dth = (diff_wheel[2] - diff_whee[0]) / L2;
+		// Backward left
+		else dth = (diff_wheel[0] - diff_wheel[2]) / L2;
+		dv = 0.0;
+		for(int i=0;i<4;i++) dv += diff_wheel[i];
+		dv /= 4.0;
+		phi = - M_PI / 4.0;
 	}
 	else if(steering_dir == 3)
 	{
+		// Left
+		if(diff_wheel[0] < 0) dth = (diff_wheel[3] - diff_wheel[0]) / L1;
+		// Right
+		else dth = (diff_wheel[2] - diff_wheel[1]) / L1;
+		dv = (-diff_wheel[0]+diff_wheel[1]+diff_wheel[2]-diff_wheel[3]) / 4.0;
+		phi = M_PI / 2.0;
 	}
 	else if(steering_dir == 4)
 	{
+		// Rotation
+		dth = (-diff_wheel[0]+diff_wheel[1]+diff_wheel[2]-diff_wheel[3]) / 4.0 / L3;
+		dv = 0.0;
+		phi = 0.0;
 	}
+		
+	cur_th += dth;
+	cur_x += dv * std::cos(theta + phi);
+	cur_y += dv * std::sin(theta + phi);
 }
 
 void WheelOdometry::publication()
